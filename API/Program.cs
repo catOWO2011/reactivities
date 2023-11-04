af796ca5ff694a3dc4dfb1dfd46d0092f0aa5f5f
catOWO2011 <catOWO2011@gmail.com>
2023-11-03 20:23:38 -0400

    Added Delete Activity Command, Created ApplicationServices extensions

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,4 @@
-using Application.Activities;
-using Application.Core;
+using API.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -8,30 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    string pgHost = Environment.GetEnvironmentVariable("DATABASE_HOST");
-    string pgPort = Environment.GetEnvironmentVariable("DATABASE_PORT");
-    string pgUser = Environment.GetEnvironmentVariable("DATABASE_USER");
-    string pgPass = Environment.GetEnvironmentVariable("DATABASE_PASSWORD");
-    string pgDb = Environment.GetEnvironmentVariable("DATABASE_NAME");
-
-    string connectionString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
-
-    opt.UseNpgsql(connectionString);
-});
-builder.Services.AddCors(opt =>
-{
-    opt.AddPolicy("CorsPolicy", policy =>
-    {
-        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-    });
-});
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(List.Handler).Assembly));
-builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
